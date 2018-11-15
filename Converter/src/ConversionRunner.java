e0ca7676cb18d08a1a832435fb215d043c1f4e30
Junior.rodriguez <Junior.rodriguez@hectors-imac.hsd1.co.comcast.net>
2018-11-14 21:30:54 -0700

    CHANGES
    I split up the Time Conversions Method into two seperate methods. Not
    sure whether its a good idea or not though so just let me know about
    your thoughts.

--- Converter/src/ConversionRunner.java
+++ Converter/src/ConversionRunner.java
@@ -40,47 +40,61 @@ public class ConversionRunner
   
   public static void timeConversion()
 		{
-			String[] timeUnits = {"Milliseconds", "Seconds", "Minutes", "Hours", "Days", "Weeks", "Years", "Decades", "Centuries"};
-			boolean choosingStartingUnit = true;
-			String startingUnit = "";
-			while (choosingStartingUnit)
-				{
-					System.out.println("What's your unit of time you want to convert from?");
-					for (String s: timeUnits)
-						{
-							System.out.println(s);
-						}
-					Scanner userInput = new Scanner (System.in);
-					startingUnit = userInput.nextLine();
-					for (String s: timeUnits)
-						{
-							if (startingUnit.toLowerCase().equals(s.toLowerCase()))
-								choosingStartingUnit = false;
-						}
-				}
-			
-			boolean choosingEndingUnit = true;
-			boolean sameUnit = false;
-			while (choosingEndingUnit)
-				{
-					System.out.println("What's your unit of time you want to convert to?");
-					for (String s: timeUnits)
-						{
-							System.out.println(s);
-						}
-					Scanner userInput = new Scanner (System.in);
-					String endingUnit = userInput.nextLine();
-					for (String s: timeUnits)
-						{
-							if (endingUnit.toLowerCase().equals(s.toLowerCase()))
-								{
-									if (endingUnit.toLowerCase().equals(startingUnit.toLowerCase()))
-										sameUnit = true;
-									
-									else
-										choosingStartingUnit = false;
-								}
-						}
-				}
+			String beginningUnit = chooseBeginningUnit();
+			String endingUnit = chooseEndingUnit();
 		}
+	
+  public static String chooseBeginningUnit()
+	  {
+		String[] timeUnits = {"Milliseconds", "Seconds", "Minutes", "Hours", "Days", "Weeks", "Years", "Decades", "Centuries"};
+		boolean choosingStartingUnit = true;
+		String startingUnit = "";
+		while (choosingStartingUnit)
+			{
+				System.out.println("What's your unit of time you want to convert from?");
+				for (String s: timeUnits)
+					{
+						System.out.println(s);
+					}
+				Scanner userInput = new Scanner (System.in);
+				startingUnit = userInput.nextLine();
+				for (String s: timeUnits)
+					{
+						if (startingUnit.toLowerCase().equals(s.toLowerCase()))
+							choosingStartingUnit = false;
+					}
+			}
+		return startingUnit;
+	  }
+  
+  public static String chooseEndingUnit(String startingUnit)
+	  	{
+		  boolean choosingEndingUnit = true;
+		  boolean sameUnit = false;
+		  while (choosingEndingUnit)
+			  {
+				  System.out.println("What's your unit of time you want to convert to?");
+				  for (String s: timeUnits)
+					  {
+						  System.out.println(s);
+					  }
+				  Scanner userInput = new Scanner (System.in);
+				  String endingUnit = userInput.nextLine();
+				  for (String s: timeUnits)
+					  {
+						  if (endingUnit.toLowerCase().equals(s.toLowerCase()))
+							  {
+								  if (endingUnit.toLowerCase().equals(startingUnit.toLowerCase()))
+									  sameUnit = true;
+										
+								  else
+									  choosingStartingUnit = false;
+							  }
+					  }
+			  }
+		  return endingUnit;
+	  	}
+  
 	}
+
+
