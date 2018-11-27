f21615ea5c32f6bc25e820cba262e02ad6b5947e
Kexin.Yu <Kexin.Yu@MU-C1-19.mullen.pvt.k12.co.us>
2018-11-27 08:29:12 -0700

    update11/27

--- Converter/src/ConversionRunner.java
+++ Converter/src/ConversionRunner.java
@@ -5,9 +5,10 @@ public class ConversionRunner
 		static Scanner userInput_int = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-				greetUser();
-				int userChoice = pickConversionType();
-				System.out.println(userChoice);
+				weightConversion();
+//				greetUser();
+//				int userChoice = pickConversionType();
+//				System.out.println(userChoice);
 			}
 		public static void greetUser()
 			{
@@ -19,15 +20,16 @@ public class ConversionRunner
 			System.out.println("\nWhat kind of conversions would you like to do today?"
 					+ "\n 1) Temperature"
 					+ "\n 2) Number Systems"
-					+ "\n 3) Distance");
+					+ "\n 3) Distance"
+					+ "\n 4) Weight");
 			int choice = userInput_int.nextInt();
-			if(choice > 0 && choice < 4)
+			if(choice > 0 && choice < 5)
 			{
 				return choice;
 			}
 			return 0;
 		}
-  
+ 
   public static void timeConversion()
 		{
 			String[] timeUnits = {"Milliseconds", "Seconds", "Minutes", "Hours", "Days", "Weeks", "Years", "Decades", "Centuries"};
@@ -88,6 +90,8 @@ public class ConversionRunner
 		  }
 		  Scanner userInput = new Scanner(System.in);
 		  startingUnit = userInput.nextLine();
+		  
+		  
 		  for(String a : weightunits) {
 			  if (startingUnit.toLowerCase().equals(a.toLowerCase()))
 					choosingStartWeight = false;
@@ -95,6 +99,14 @@ public class ConversionRunner
 		  
 		  
 	  }
+	 
+	  
+	  System.out.println("Put in a value of this weight.");
+	  Scanner userInput2 = new Scanner(System.in);
+	  double startingValue = userInput2.nextDouble();
+	  
+	 
+	  
 	  boolean choosingEndWeight = true;
 	  boolean sameUnit = false;
 	  while(choosingEndWeight) {
@@ -112,14 +124,134 @@ public class ConversionRunner
 						
 						else
 							choosingStartWeight = false;
+						
+						
 					}
 			 
 		  }
-	  }  
-  
+		  
+		  if(startingUnit.equals("kilogram")) {
+			   if(endingUnit.equals("gram")) {
+				   double endingValue1 = startingValue*1000;
+				   System.out.println("The result is "+ endingValue1 + " grams");
+			   }
+			   if(endingUnit.equals("ton")) {
+				   double endingValue2 = startingValue/1000;
+				   System.out.println("The result is "+ endingValue2 + " tons");
+			   }
+			   if(endingUnit.equals("pound")) {
+				   double endingValue3 = startingValue*2.20462;
+				   System.out.println("The result is "+ endingValue3 + " pounds");
+			   }
+			   
+			   if(endingUnit.equals("ounce")) {
+				   double endingValue4 = startingValue*35.274;
+				   System.out.println("The result is "+ endingValue4 + " ounces");
+			   } 
+			   if(sameUnit=true) {
+				   System.out.println("The result is "+ startingValue + " kilograms");
+			   }
+			  
+			  }
+		  
+		  
+		  if(startingUnit.equals("gram")) {
+			   if(endingUnit.equals("kilogram")) {
+				   double endingValue5 = startingValue/1000;
+				   System.out.println("The result is "+ endingValue5 + " grams");
+			   }
+			   if(endingUnit.equals("ton")) {
+				   double endingValue6 = startingValue/1000000;
+				   System.out.println("The result is "+ endingValue6 + " tons");
+			   }
+			   if(endingUnit.equals("pound")) {
+				   double endingValue7 = startingValue/2.20462;
+				   System.out.println("The result is "+ endingValue7 + " pounds");
+			   }
+			   
+			   if(endingUnit.equals("ounce")) {
+				   double endingValue8 = startingValue/35.274;
+				   System.out.println("The result is "+ endingValue8 + " ounces");
+			   }
+			   if(sameUnit=true) {
+				   System.out.println("The result is "+ startingValue + " grams");
+			   }
+			  }
+		  
+		  if(startingUnit.equals("ton")) {
+			   if(endingUnit.equals("kilogram")) {
+				   double endingValue5 = startingValue*1000;
+				   System.out.println("The result is "+ endingValue5 + " kilograms");
+			   }
+			   if(endingUnit.equals("gram")) {
+				   double endingValue6 = startingValue*1000000;
+				   System.out.println("The result is "+ endingValue6 + " grams");
+			   }
+			   if(endingUnit.equals("pound")) {
+				   double endingValue7 = startingValue*2000;
+				   System.out.println("The result is "+ endingValue7 + " pounds");
+			   }
+			   
+			   if(endingUnit.equals("ounce")) {
+				   double endingValue8 = startingValue*32000;
+				   System.out.println("The result is "+ endingValue8 + " ounces");
+			   }
+			   if(sameUnit=true) {
+				   System.out.println("The result is "+ startingValue + " tons");
+			   }
+			  }
+		  
+		  if(startingUnit.equals("pound")) {
+			   if(endingUnit.equals("kilogram")) {
+				   double endingValue5 = startingValue*0.453592;
+				   System.out.println("The result is "+ endingValue5 + " kilograms");
+			   }
+			   if(endingUnit.equals("gram")) {
+				   double endingValue6 = startingValue*453.592;
+				   System.out.println("The result is "+ endingValue6 + " grams");
+			   }
+			   if(endingUnit.equals("ton")) {
+				   double endingValue7 = startingValue*0.0005;
+				   System.out.println("The result is "+ endingValue7 + "tons");
+			   }
+			   
+			   if(endingUnit.equals("ounce")) {
+				   double endingValue8 = startingValue*16;
+				   System.out.println("The result is "+ endingValue8 + " ounces");
+			   }
+			   if(sameUnit=true) {
+				   System.out.println("The result is "+ startingValue + " pounds");
+			   }
+			  }
+		  
+		  if(startingUnit.equals("ounce")) {
+			   if(endingUnit.equals("kilogram")) {
+				   double endingValue5 = startingValue*0.0283495;
+				   System.out.println("The result is "+ endingValue5 + " kilograms");
+			   }
+			   if(endingUnit.equals("gram")) {
+				   double endingValue6 = startingValue*28.3495;
+				   System.out.println("The result is "+ endingValue6 + " grams");
+			   }
+			   if(endingUnit.equals("ton")) {
+				   double endingValue7 = startingValue*0.000031249974509375;
+				   System.out.println("The result is "+ endingValue7 + "tons");
+			   }
+			   
+			   if(endingUnit.equals("pound")) {
+				   double endingValue8 = startingValue*0.062499949018750007579;
+				   System.out.println("The result is "+ endingValue8 + " pounds");
+			   }
+			   if(sameUnit=true) {
+				   System.out.println("The result is "+ startingValue + " ounces");
+			   }
+			   
+			  }
+	  }
+	 
   }
   
-  
+
   
   
 	}
